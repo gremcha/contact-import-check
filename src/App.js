171eef21848218e7a52f8d30e2fb460e3e783be3
gremcha <kris123313@gmail.com>
2024-11-07 16:57:23 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import { isYandex } from 'react-device-detect'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const checkContactApiSupport = () => {
+    return navigator.contacts && window.ContactsManager && !isYandex
 }
 
-export default App;
+async function pickContacts() {
+    try {
+        const propertiesAvailable = await navigator.contacts.getProperties()
+
+        const contacts = await navigator.contacts.select(propertiesAvailable, {
+            multiple: true,
+        })
+        return contacts
+    } catch (e) {
+        console.log(e)
+        alert('Не удалось получить контакты')
+        return []
+    }
+}
+function App() {
+    const [supportContactApi, setSupportContactApi] = useState()
+    const [contacts, setContacts] = useState([])
+    return (
+        <div className="App">
+            <div className="block">
+                <h3>Проверить поддержку api в браузере</h3>
+                <span>Результат: {`${supportContactApi}`}</span>
+                <button
+                    onClick={() =>
+                        setSupportContactApi(checkContactApiSupport())
+                    }
+                >
+                    Проверить
+                </button>
+            </div>
+            <div className="block">
+                <h3>Получить контакты</h3>
+                <span>Импортировано контактов: {contacts.length}</span>
+                <button onClick={async () => setContacts(await pickContacts())}>
+                    Получить
+                </button>
+                {!!contacts.length &&
+                    contacts.map((contact) => JSON.stringify(contact))}
+            </div>
+        </div>
+    )
+}
+
+export default App
